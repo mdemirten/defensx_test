dabac26124ad4b197674d745062969891b239b59
Mursel <mursel@defensx.com>
2025-04-07 11:57:37 +0300

    alt sayfa

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,10 +10,18 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  var animation = lottie.loadAnimation({
+    container: document.getElementById('lottie-container-mobile'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/defensx_test/assets/animations/DefensX_Mobile_Animation_V2.json'
+  });
+});
+
 
 $(document).ready(function(){
-  "use strict";
-
   var owl = $('.brands-carousel-6');
   owl.owlCarousel({
     items: 5,
@@ -24,6 +32,83 @@ $(document).ready(function(){
     autoplayTimeout: 4000,
     autoplayHoverPause: false,
     smartSpeed: 2000,
+    responsive:{
+      0:{
+        items:2
+      },
+      550:{
+        items:3
+      },
+      767:{
+        items:3
+      },
+      768:{
+        items:5
+      },
+      991:{
+        items:6
+      },				
+      1000:{
+        items:6
+      }
+    }
+});
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+});
+
+
+$(document).ready(function(){
+  var owl = $('.brands-carousel-7');
+  owl.owlCarousel({
+    items: 5,
+    loop:true,
+    autoplay:true,
+    navBy: 1,
+    nav:false,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: false,
+    smartSpeed: 4000,
+    responsive:{
+      0:{
+        items:2
+      },
+      550:{
+        items:3
+      },
+      767:{
+        items:3
+      },
+      768:{
+        items:5
+      },
+      991:{
+        items:6
+      },				
+      1000:{
+        items:6
+      }
+    }
+});
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+});
+
+$(document).ready(function(){
+  var owl = $('.brands-carousel-8');
+  owl.owlCarousel({
+    items: 5,
+    loop:true,
+    autoplay:true,
+    navBy: 1,
+    nav:false,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: false,
+    smartSpeed: 4000,
     responsive:{
       0:{
         items:2
